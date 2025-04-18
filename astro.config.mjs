d4e7e33329a8dbbff810976cb7a683b16d662b58
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2025-04-18 10:18:28 -0400

    lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	vite: {
+		// @ts-ignore
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [alpinejs()]
+	integrations: [alpinejs()],
 });
